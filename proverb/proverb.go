558dfb72e32d637758e59de2c32b4f4af2b9c23b
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-01-16 18:51:36 -0700

    passing tests, kinda interesting problem...

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,15 +1,22 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package provide includes a functin Proverb which
+//	takes an arbitrary numbers of string
+//	and constructs a proverb.
 package proverb
 
-// Proverb should have a comment documenting it.
+// Proverb takes an arbitrary number of strings and constructs
+//	a proverb.
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	proverb := []string{}
+	// catch zero-length case
+	if len(rhyme) == 0 {
+		return proverb
+	}
+	for i, value := range rhyme {
+		if i < len(rhyme)-1 {
+			proverb = append(proverb, "For want of a "+value+" the "+rhyme[i+1]+" was lost.")
+		} else {
+			proverb = append(proverb, "And all for the want of a "+rhyme[0]+".")
+		}
+	}
+	return proverb
 }
