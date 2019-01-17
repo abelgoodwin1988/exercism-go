2ec634791aa388128d60fc7f1bfea664956834a6
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-01-16 17:35:27 -0700

    add leapyears function

--- leap/leap.go
+++ leap/leap.go
@@ -1,15 +1,13 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package leap exports a function which determines
+//	whether or not a provided year is a leap year.
 package leap
 
-// IsLeapYear should have a comment documenting it.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
+// IsLeapYear accepts an in representing a year of the
+//	gregorian calendar and returns a a boolean for whether
+//	it is or is not a leap year.
+func IsLeapYear(year int) bool {
+	if year%4 == 0 && (year%100 != 0 || year%400 == 0) {
+		return true
+	}
 	return false
 }
