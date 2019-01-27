4fc34d442a34ee5064fae23a2c6f0ddfcd955d00
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-01-26 17:36:49 -0700

    used formulas instead of loops

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -5,20 +5,12 @@ import "math"
 
 // SquareOfSum takes an int and return the square of the sum
 func SquareOfSum(n int) int {
-	var squareOfSums int
-	for i := 1; i <= n; i++ {
-		squareOfSums += i
-	}
-	return int(math.Pow(float64(squareOfSums), 2))
+	return int(math.Pow(float64(n*(n+1)/2), 2.0))
 }
 
 // SumOfSquares takes an int and returns the sum of n's squared
 func SumOfSquares(n int) int {
-	var sumOfSquares int
-	for i := 1; i <= n; i++ {
-		sumOfSquares += int(math.Pow(float64(i), 2))
-	}
-	return sumOfSquares
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 // Difference returns the difference between the two calculations
