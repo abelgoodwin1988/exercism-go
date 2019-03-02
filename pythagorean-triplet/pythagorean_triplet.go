fc932247f02b60234e8a3bd721cdb0727c9128df
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-03-01 21:41:34 -0700

    solved

--- pythagorean-triplet/pythagorean_triplet.go
+++ pythagorean-triplet/pythagorean_triplet.go
@@ -1,7 +1,41 @@
-// Package pythagorean ...
+// Package pythagorean contains method(s) Sum which sums a
+//	pythagorean triplet
 package pythagorean
 
-// DeepEqual ...
-func DeepEqual() {
-	return
+// Triplet is a struct representing the value used
+//	in a pythagorean triplet
+type Triplet struct {
+	a int
+	b int
+	c int
+}
+
+// Sum accepts a sum of pythagorean a triplet. It then calculates
+//	and returns all possible pythagorean triplets.
+func Sum(p int) []Triplet {
+	t := []Triplet{}
+	for a := 2; a < p/3; a++ {
+		for b := a + 1; b < p/2; b++ {
+			c := p - a - b
+			if a*a+b*b == c*c {
+				t = append(t, Triplet{a, b, c})
+			}
+		}
+	}
+	return t
+}
+
+// Range ...
+func Range(min, max int) []Triplet {
+	t := []Triplet{}
+	for a := min; a <= max; a++ {
+		for b := a + 1; b <= max; b++ {
+			for c := b + 1; c <= max; c++ {
+				if a*a+b*b == c*c {
+					t = append(t, Triplet{a, b, c})
+				}
+			}
+		}
+	}
+	return t
 }
