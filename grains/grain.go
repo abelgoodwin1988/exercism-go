725e34cdb37bfa5519da2d271d3b96210cee9eff
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-03-02 13:16:23 -0700

    add and complete exercism exercise

---
+++ grains/grain.go
@@ -0,0 +1,36 @@
+// Package grains contains method Square and Total which
+//	return the number of grains on an nth sqaure on a
+//	chessboard, or a the sum of grains on a chessboard
+package grains
+
+import (
+	"errors"
+	"math"
+)
+
+// Square accepts an integer representing the nth square
+//	on a chessboard and returns a sum of powers representing
+//	numbers of grains on the board following the pattern
+//	in the readme.md
+func Square(n int) (uint64, error) {
+	// Catch out-of-bounds errors
+	if n < 1 || n > 64 {
+		return 0, errors.New("out-of-range chess-board square value")
+	}
+	if n < 3 {
+		return uint64(n), nil
+	}
+	return uint64(math.Pow(2, float64(n)-1)), nil
+}
+
+// Total returns the total number of grains of rice
+//	found on a chess-board following the doubling algorithm
+func Total() uint64 {
+	var sum uint64
+	j := uint64(1)
+	for i := uint64(1); i < 64; i++ {
+		j *= 2
+		sum += j
+	}
+	return 1 + sum
+}
