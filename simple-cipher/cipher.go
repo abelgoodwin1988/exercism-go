42819b419c460b40fe1dbed0c012b6465b17a1ac
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-03-17 01:38:29 -0600

    complete encode & decode for shift cipher

--- simple-cipher/cipher.go
+++ simple-cipher/cipher.go
@@ -26,10 +26,13 @@ type Cipher interface {
 // Series of structs used for ciphering which will implement the
 //	Cipher interface
 
-// Caesar ...
+// Caesar struct to implement the encoding and decoding of a Caeser Cipher,
+//	which implements the Cipher interface
 type Caesar struct{}
 
-// Shift ...
+// Shift struct to implement the encoding and decoding of a Shift Cipher,
+//	which implements the Cipher interface. The Distance field determine how
+//	many alphabet characters to shift.
 type Shift struct {
 	distance int
 }
@@ -42,13 +45,22 @@ type Vigenere struct {
 // Series of methods for returning new structs
 //	which will be used to implement the Cipher interface
 
-// NewCaesar ...
+// NewCaesar returns Caesar structs which implements the Cipher interface.
+//	The Caesar cipher always shifts the alphabetical (english in this case) character three ahead.
+//	In the case of extending beyond the last character, the cipher wraps to the beginning
+//	of the alphabet
 func NewCaesar() *Caesar {
 	return &Caesar{}
 }
 
-// NewShift ...
+// NewShift returns a new Shift struct which implements the Cipher interface.
+//	the Shift cipher shifts n characters based on the defined distance field.
+//	Shift accepts negative and positive values, but has bounds based on the
+//	English alphabet of 26 and cannot shift 0.
 func NewShift(distance int) *Shift {
+	if distance <= -26 || distance >= 26 || distance == 0 {
+		return nil
+	}
 	return &Shift{distance: distance}
 }
 
@@ -57,7 +69,8 @@ func NewVigenere(key string) *Vigenere {
 	return &Vigenere{key: key}
 }
 
-// Encode ...
+// Encode Caesar returns ciphered string by shifting ahead three characters
+//	and wrapping when exceeding the English alphabet character upper-bound of z.
 func (c *Caesar) Encode(s string) string {
 	normalizedS := strings.Map(Normalize, s)
 	cipheredS := strings.Map(
@@ -70,7 +83,8 @@ func (c *Caesar) Encode(s string) string {
 	return cipheredS
 }
 
-// Encode ...
+// Encode Shift returns ciphered string by shifting the Shift structs set distance
+//	field. Wraps when exceeding upper bound or below lower bound.
 func (c *Shift) Encode(s string) string {
 	normalizedS := strings.Map(Normalize, s)
 	cipheredS := strings.Map(
@@ -78,7 +92,7 @@ func (c *Shift) Encode(s string) string {
 			switch {
 			case c.distance < 0:
 				if r+rune(c.distance) < 'a' {
-					return 'z' - (r+rune(c.distance))%26
+					return 'z' + 1 + (r + rune(c.distance) - 'a')
 				}
 				return r - rune(c.distance*-1)
 			default:
@@ -98,7 +112,9 @@ func (c *Vigenere) Encode(s string) string {
 	return ""
 }
 
-// Decode ...
+// Decode Caesar returns deciphered string which used the Encode Caesar method.
+//	This is accomplished by going back three english alphabet characters with
+//	alphabet wrapping
 func (c *Caesar) Decode(s string) string {
 	normalizedS := strings.Map(Normalize, s)
 	cipheredS := strings.Map(
@@ -111,13 +127,22 @@ func (c *Caesar) Decode(s string) string {
 	return cipheredS
 }
 
-// Decode ...
+// Decode Shift returns deciphered string which used the Encode Shift method.
+//	This is accomplished by going the opposite value defined in the Shift struct.
+//	Alphabet wrapping is present.
 func (c *Shift) Decode(s string) string {
 	normalizedS := strings.Map(Normalize, s)
 	cipheredS := strings.Map(
 		func(r rune) rune {
-			if r-rune(c.distance) < 'a' {
-				return ('a' - r) + 'z'
+			switch {
+			case c.distance < 0:
+				if r-rune(c.distance) > 'z' {
+					return 'a' + (r-rune(c.distance)-'a')%26
+				}
+			default:
+				if r-rune(c.distance) < 'a' {
+					return ('a' - r) + 'z'
+				}
 			}
 			return r - rune(c.distance)
 		}, normalizedS)
