89f104e959b514c04ec4ebf763397f731075219e
Abel Goodwin <abelgoodwin1988@gmail.com>
2019-04-08 19:29:17 -0600

    redoing my grep because it was terrible. really terrible

--- grep/grep.go
+++ grep/grep.go
@@ -7,168 +7,48 @@ import (
 	"bufio"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
 
-// create constant holding map of false strings; when true we'll
-//	enable the features for the flags
-var flagsMap = map[string]bool{
-	"-n": false,
-	"-l": false,
-	"-i": false,
-	"-v": false,
-	"-x": false,
-}
+// flag readable names
+var (
+	printLineNumber      = "-n"
+	printNameOnly        = "-l"
+	matchCaseInsensitive = "-i"
+	matchNonMatches      = "-v"
+	matchEntireLine      = "-x"
+)
 
-// Use readable names for mappings
-var printLineNumbers = flagsMap["-n"]
-var printFileNames = flagsMap["-l"]
-var invertMatch = flagsMap["-i"]
-var ignoreCase = flagsMap["-v"]
-var matchWholeLine = flagsMap["-x"]
-
-/*
-Search accepts a pattern string, flags string and  files []string.
-
-Search will iterate through the files in the order provided and add any
-matching lines of text to the return.
-
-If the search had more than one file, each string should have the
-filename as a suffix to the matching line.
-
--- Flags
-- -n Print the line numbers of each matching line.
-- -l Print only the names of files that contain at least one matching line.
-- -v Invert the program -- collect all lines that fail to match the pattern.
-- -i Match line using a case-insensitive comparison.
-- -x Only match entire lines, instead of lines that contain a match.
-*/
-func Search(pattern string, flags []string, files []string) (matches []string) {
-	// instantiate empty matches for match
-	matches = []string{}
-	// multiple files?
-	multipleFiles := len(files) > 1
-	// reset mapping
-	for i := range flagsMap {
-		flagsMap[i] = false
-	}
-	// set mapping for current search
-	for _, value := range flags {
-		flagsMap[value] = true
-	}
-	// set simple names for current search
-	printLineNumbers = flagsMap["-n"]
-	printFileNames = flagsMap["-l"]
-	invertMatch = flagsMap["-i"]
-	ignoreCase = flagsMap["-v"]
-	matchWholeLine = flagsMap["-x"]
-	// iterate through list of file names
-	for _, file := range files {
-		// Open the file
-		f, err := os.Open(file)
+// Search accepts a pattern, flags and filenames. Search will match the pattern
+//	to the lines in the files while applying any matching rules found in the flags.
+//	Flags:
+// - `-n` Print the line numbers of each matching line.
+// - `-l` Print only the names of files that contain at least one matching line.
+// - `-i` Match line using a case-insensitive comparison.
+// - `-v` Invert the program -- collect all lines that fail to match the pattern.
+// - `-x` Only match entire lines, instead of lines that contain a match.
+func Search(pattern string, flags []string, files []string) []string {
+	match := []string{}
+	for _, fileName := range files {
+		file, err := os.Open(fileName)
 		if err != nil {
-			log.Fatalf("Error opening file %v: \n%v\n", file, err)
-			return nil
+			log.Printf("Error opening file %v with error %v\n", fileName, err)
 		}
-		defer f.Close()
-		// Read the file
-		scanner := bufio.NewScanner(f)
-		var fileLineArray []string
-		// Send each line to an array
+		defer file.Close()
+
+		// Read each line by line and evaluate for matches based on flags
+		scanner := bufio.NewScanner(file)
+		i := 0
 		for scanner.Scan() {
-			fileLineArray = append(fileLineArray, scanner.Text())
-		}
-
-		// Iterate through array, check for matches, assign to
-		//	return string array.
-		for lineNumber, value := range fileLineArray {
-			if ignoreCase {
-				if invertMatch && matchWholeLine {
-					insensitivePattern := strings.ToLower(pattern)
-					insensitiveValue := strings.ToLower(value)
-					if insensitivePattern != insensitiveValue {
-						matches = matched(matches, file, value, lineNumber, multipleFiles)
-					}
-					continue
-				}
-				if invertMatch {
-					insensitivePattern := strings.ToLower(pattern)
-					insensitiveValue := strings.ToLower(value)
-					if !strings.Contains(insensitiveValue, insensitivePattern) {
-						matches = matched(matches, file, value, lineNumber, multipleFiles)
-					}
-					continue
-				}
-				if matchWholeLine {
-					if value != pattern {
-						matches = matched(matches, file, value, lineNumber, multipleFiles)
-					}
-					continue
-				}
-				if !strings.Contains(value, pattern) {
-					matches = matched(matches, file, value, lineNumber, multipleFiles)
-				}
-				continue
-			} else {
-				if invertMatch && matchWholeLine {
-					insensitivePattern := strings.ToLower(pattern)
-					insensitiveValue := strings.ToLower(value)
-					if insensitivePattern == insensitiveValue {
-						matches = matched(matches, file, value, lineNumber, multipleFiles)
-					}
-					continue
-				}
-				if invertMatch {
-					insensitivePattern := strings.ToLower(pattern)
-					insensitiveValue := strings.ToLower(value)
-					if strings.Contains(insensitiveValue, insensitivePattern) {
-						matches = matched(matches, file, value, lineNumber, multipleFiles)
-					}
-					continue
-				}
-				if matchWholeLine {
-					if value == pattern {
-						matches = matched(matches, file, value, lineNumber, multipleFiles)
-					}
-					continue
-				}
-				if strings.Contains(value, pattern) {
-					matches = matched(matches, file, value, lineNumber, multipleFiles)
-				}
-				continue
+			line := scanner.Text()
+			if strings.Contains(line, pattern) {
+				match = append(match, line)
 			}
 		}
-	}
-	return matches
-}
 
-func matched(matches []string, file string, value string, lineNumber int, multipleFiles bool) (rtnMatches []string) {
-	var matchLine string
-	if printFileNames {
-		for _, fileName := range matches {
-			if fileName == file {
-				return matches
-			}
+		if err := scanner.Err(); err != nil {
+			log.Printf("Error reading file %v at line %v with error %v\n", fileName, i, err)
 		}
-		matchLine += file
-		rtnMatches = append(matches, matchLine)
-		return rtnMatches
 	}
-	if multipleFiles {
-		matchLine += file + ":"
-
-	}
-	if flagsMap["-n"] {
-		matchLine += strconv.Itoa(lineNumber+1) + ":"
-	}
-	if multipleFiles {
-		matchLine += value
-		rtnMatches = append(matches, matchLine)
-		return rtnMatches
-	}
-
-	matchLine += value
-	rtnMatches = append(matches, matchLine)
-	return rtnMatches
+	return match
 }
